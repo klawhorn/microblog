63dfa05ff20233b869da8b3f47e52c8cf86ab48b
Katie <lawhorn.pdx@gmail.com>
2015-06-23 15:52:21 -0700

    About to change the redis cache to be knex/postgres db for the tweetTable

--- routes/redis-demo.js
+++
@@ -1,8 +0,0 @@
-var redis = require('redis');
-var client = redis.createClient();
-
-client.set("key", "value", function () {
-	client.get("bnanana", function(err,res) {
-		console.log('done'+res);
-	})
-})
