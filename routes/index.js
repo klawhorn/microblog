9d807350eca59725f93a7ec554c35bdf5de8e385
Katie <lawhorn.pdx@gmail.com>
2015-07-02 16:45:01 -0700

    another fucking redis

--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,10 @@ if (process.env.REDISTOGO_URL) {
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
     var cache = require("redis").createClient(rtg.port, rtg.hostname);
 
-    redis.auth(rtg.auth.split(":")[1]);
+    cache.auth(rtg.auth.split(":")[1]);
 
 } else {
-    var redis = require("redis").createClient();
+    var cache = require("redis").createClient();
 }
 
 /***************************************************
